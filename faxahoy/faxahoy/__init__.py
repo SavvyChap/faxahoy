a9fb3c298a74697edecc3a6422e2c7e50c48c936
august1073 <augustmoney81@gmail.com>
2017-11-27 08:49:51 -0600

    adding db
    adding db

--- faxahoy/faxahoy/__init__.py
+++ faxahoy/faxahoy/__init__.py
@@ -3,9 +3,11 @@ The flask application package.
 """
 
 from flask import Flask
+from flask_sqalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+db = SQLAlchemy(app)
 
 import faxahoy.views
