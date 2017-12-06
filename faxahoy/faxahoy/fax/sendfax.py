d7b535fda26c39d1bacbb6ac58b3c32a17b443cc
August Money <augustmoney@live.com>
2017-12-05 18:38:36 -0600

    Setup receive fax and send as email with attachment

--- faxahoy/faxahoy/fax/sendfax.py
+++ faxahoy/faxahoy/fax/sendfax.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client
 
-account_sid = "-----"
-auth_token = "-----"
+account_sid = "----"
+auth_token = "----"
 
 client = Client(account_sid, auth_token)
 
 fax = client.fax.v1.faxes.create(
-    from_="----",
-    to="----",
+    from_="+17132367743",
+    to="+13104992491",
     media_url="https://www.twilio.com/docs/documents/25/justthefaxmaam.pdf")
 
 print(fax.sid)
